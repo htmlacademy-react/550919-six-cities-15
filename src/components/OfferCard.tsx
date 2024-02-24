6f566c9017f179f7f731e0f001342bfd5c883af8
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2024-02-24 12:53:31 +0300

    fix errors

--- src/components/OfferCard.tsx
+++ src/components/OfferCard.tsx
@@ -1,33 +1,19 @@
 import React from 'react';
 
-interface OfferCardProps {
-  premium: boolean;
-  // image: string;
-  price: number;
-  rating: number;
-  title: string;
-  type: string;
-}
-
-// пока хардкодом, чтоб просто картинка была хоть какая-то
-const imagePath = '/img/apartment-01.jpg';
-
-const OfferCard: React.FC<OfferCardProps> = ({ premium, price, rating, title, type }) => (
+const OfferCard: React.FC = () => (
   <article className="cities__card place-card">
-    {premium && (
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
-    )}
+    <div className="place-card__mark">
+      <span>Premium</span>
+    </div>
     <div className="cities__image-wrapper place-card__image-wrapper">
       <a href="#">
-        <img className="place-card__image" src={imagePath} width="260" height="200" alt="Place image" />
+        <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
       </a>
     </div>
     <div className="place-card__info">
       <div className="place-card__price-wrapper">
         <div className="place-card__price">
-          <b className="place-card__price-value">&euro;{price}</b>
+          <b className="place-card__price-value">&euro;120</b>
           <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
         <button className="place-card__bookmark-button button" type="button">
@@ -39,14 +25,14 @@ const OfferCard: React.FC<OfferCardProps> = ({ premium, price, rating, title, ty
       </div>
       <div className="place-card__rating rating">
         <div className="place-card__stars rating__stars">
-          <span style={{ width: `${rating * 20}%` }}></span>
+          <span style={{width: '80%'}}></span>
           <span className="visually-hidden">Rating</span>
         </div>
       </div>
       <h2 className="place-card__name">
-        <a href="#">{title}</a>
+        <a href="#">Beautiful &amp; luxurious apartment at great location</a>
       </h2>
-      <p className="place-card__type">{type}</p>
+      <p className="place-card__type">Apartment</p>
     </div>
   </article>
 );
