5b4c1852a91fc8f9f1db5239b5d87dc73fb26907
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2024-03-10 11:49:20 +0300

    исправления после проверки PR #2

--- src/components/comment-card.tsx
+++ src/components/comment-card.tsx
@@ -1,81 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Comments } from '../types/comments';
 
 type CommentCardProps = {
   comments: Comments;
 };
 
-const CommentCard: React.FC<CommentCardProps> = ({ comments }) => (
-  <div>
-    <ul className="reviews__list">
-      {comments.map((comment) => (
-        <li className="reviews__item" key={comment.id}>
-          <div className="reviews__user user">
-            <div className="reviews__avatar-wrapper user__avatar-wrapper">
-              <img className="reviews__avatar user__avatar" src={comment.user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
-            </div>
-            <span className="reviews__user-name">{comment.user.userName}</span>
-          </div>
-          <div className="reviews__info">
-            <div className="reviews__rating rating">
-              <div className="reviews__stars rating__stars">
-                <span style={{ width: `${comment.rating * 20}%` }}></span>
-                <span className="visually-hidden">Rating</span>
+const CommentCard: React.FC<CommentCardProps> = ({ comments }) => {
+  const [newComment, setNewComment] = useState('');
+  const [selectedRating, setSelectedRating] = useState<number | null>(null);
+
+  const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewComment(event.target.value);
+  };
+
+  const handleRatingChange = (value: number) => {
+    setSelectedRating(value);
+  };
+
+  return (
+    <div>
+      <ul className="reviews__list">
+        {comments.map((comment) => (
+          <li className="reviews__item" key={comment.id}>
+            <div className="reviews__user user">
+              <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                <img className="reviews__avatar user__avatar" src={comment.user.avatarUrl} width="54" height="54" alt="Reviews avatar" />
               </div>
+              <span className="reviews__user-name">{comment.user.userName}</span>
             </div>
-            <p className="reviews__text">{comment.comment}</p>
-            <time className="reviews__time" dateTime={comment.date}>{comment.date}</time>
-          </div>
-        </li>
-      ))}
-    </ul>
-    <form className="reviews__form form" action="#" method="post">
-      <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-      <div className="reviews__button-wrapper">
-        <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-        </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-      </div>
-    </form>
-  </div>
-);
+            <div className="reviews__info">
+              <div className="reviews__rating rating">
+                <div className="reviews__stars rating__stars">
+                  <span style={{ width: `${comment.rating * 20}%` }}></span>
+                  <span className="visually-hidden">Rating</span>
+                </div>
+              </div>
+              <p className="reviews__text">{comment.comment}</p>
+              <time className="reviews__time" dateTime={comment.date}>{comment.date}</time>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <form className="reviews__form form" action="#" method="post">
+        <label className="reviews__label form__label" htmlFor="review">Your review</label>
+        <div className="reviews__rating-form form__rating">
+          {Array.from({ length: 5 }, (_, index) => (
+            <React.Fragment key={index}>
+              <input
+                className="form__rating-input visually-hidden"
+                name="rating"
+                value={5 - index}
+                id={`${5 - index}-stars`}
+                type="radio"
+                checked={selectedRating === 5 - index}
+                onChange={() => handleRatingChange(5 - index)}
+              />
+              <label htmlFor={`${5 - index}-stars`} className="reviews__rating-label form__rating-label">
+                <svg className="form__star-image" width="37" height="33">
+                  <use xlinkHref="#icon-star"></use>
+                </svg>
+              </label>
+            </React.Fragment>
+          ))}
+        </div>
+        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={newComment} onChange={handleCommentChange}></textarea>
+        <div className="reviews__button-wrapper">
+          <p className="reviews__help">
+            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          </p>
+          <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default CommentCard;
