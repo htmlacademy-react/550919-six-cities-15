47a08cb83a049a77b03ff71a00fae64e4b161b15
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2024-03-21 20:23:02 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const isAuthenticated = true;
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
