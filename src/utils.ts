ac8a5bb928da6ff6935a2530b45ae9a390dc547a
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2024-03-24 17:50:06 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,14 @@
-import { Offers } from 'types/offers';
+import { Offers } from './types/offers';
 import { SortType } from './const';
 
-export function sortOffers(sortType: SortType, offers: Offers) {
+export function sortOffers(sortType: SortType, offers: Offers): Offers {
   switch (sortType) {
     case SortType.LowToHigh:
-      return offers.sort((a, b) => a.price - b.price);
+      return offers.slice().sort((a, b) => a.price - b.price);
     case SortType.HighToLow:
-      return offers.sort((a, b) => b.price - a.price);
+      return offers.slice().sort((a, b) => b.price - a.price);
     case SortType.TopRated:
-      return offers.sort((a, b) => b.rating - a.rating);
-    case SortType.Popular:
-      return offers;
+      return offers.slice().sort((a, b) => b.rating - a.rating);
     default:
       return offers;
   }
